33286122b651570ab3e714c8b49c35fa6ef7bcb9
Vadim Markovtsev <vadim@sourced.tech>
2019-10-17 16:55:41 +0200

    Hard-fork
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 import io
 import os
 
+from Cython.Build import cythonize
 from setuptools import Extension, find_packages, setup
 
-extensions = [
+extensions = cythonize([
     Extension(
         "_youtokentome_cython",
         [
-            "youtokentome/cpp/yttm.cpp",
+            "youtokentome/cpp/yttm.pyx",
             "youtokentome/cpp/bpe.cpp",
             "youtokentome/cpp/utils.cpp",
             "youtokentome/cpp/utf8.cpp",
         ],
-        extra_compile_args=["-std=c++11", "-pthread", "-O3", "-static"],
+        extra_compile_args=["-std=c++14", "-pthread", "-O3", "-static", "-flto"],
         language="c++",
     )
-]
+])
 
 with io.open(
     os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"),
@@ -24,17 +25,17 @@ with io.open(
     LONG_DESCRIPTION = "\n" + f.read()
 
 setup(
-    name="youtokentome",
-    version="1.0.2",
+    name="youtokentome-srcd",
+    version="2.0.0",
     packages=find_packages(),
     description="Unsupervised text tokenizer focused on computational efficiency",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/vkcom/youtokentome",
+    url="https://github.com/src-d/youtokentome",
     python_requires=">=3.5.0",
     install_requires=["Click>=7.0"],
     entry_points={"console_scripts": ["yttm = youtokentome.yttm_cli:main"]},
-    author="Ivan Belonogov",
+    author="Ivan Belonogov; source{d}",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
@@ -47,5 +48,15 @@ setup(
         "Programming Language :: Cython",
         "Programming Language :: C++",
     ],
+    package_data={"": [
+        "youtokentome/cpp/utils.h",
+        "youtokentome/cpp/bpe.h",
+        "youtokentome/cpp/utf8.h",
+        "youtokentome/cpp/third_party/flat_hash_map.h",
+        "youtokentome/cpp/third_party/LICENSE",
+        "LICENSE",
+        "README.md",
+        "requirements.txt",
+    ],},
     ext_modules=extensions,
 )
